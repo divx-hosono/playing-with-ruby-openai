44da5ff520dc4aede4271881f062ae92ecd0dd33
S-Hosono@divx <shumpei.hosono@divx.co.jp>
2023-05-17 15:43:52 +0900

    他ファイルのrequireをまとめた

--- sample_cli.rb
+++ sample_cli.rb
@@ -1,15 +1,8 @@
 require 'thor'
 require_relative './config/openai.rb'
-require_relative './ruby_openai/chat_gpt.rb'
-require_relative './ruby_openai/completion.rb'
-require_relative './ruby_openai/edit.rb'
-require_relative './ruby_openai/embedding.rb'
-require_relative './ruby_openai/file.rb'
-require_relative './ruby_openai/finetune.rb'
-require_relative './ruby_openai/image.rb'
-require_relative './ruby_openai/moderation.rb'
-require_relative './ruby_openai/transcribe.rb'
-require_relative './ruby_openai/translate.rb'
+Dir[File.expand_path("../ruby_openai", __FILE__) << "/*.rb"].each do |file|
+  require file
+end
 
 class SampleCLI < Thor
   desc "chat", "ChatGPT API"
