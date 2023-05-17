da3a79e1f3f5aa00ef71eb7836c909f5f701d341
S-Hosono@divx <shumpei.hosono@divx.co.jp>
2023-05-17 15:34:41 +0900

    embeddingメソッド追加

--- sample_cli.rb
+++ sample_cli.rb
@@ -18,6 +18,13 @@ class SampleCLI < Thor
     chat_gpt = RubyOpenAI::ChatGPT.new(client, model_version)
     response = chat_gpt.get_response(messages: [{ role: "user", content: input_message}])
     puts response
+    while retry? do
+      puts "Please input your message."
+      input = input_message
+      response = chat_gpt.get_response(messages: [{ role: "user", content: input_message}])
+      puts response
+    end
+    thanks_message
   end
 
   desc "completion", "Completion API"
@@ -46,12 +53,22 @@ class SampleCLI < Thor
     thanks_message
   end
 
+  desc "embedding", "Embedding API"
+  def embedding
+    puts "Please input your message."
+    embedding = RubyOpenAI::Embedding.new(client, model_version("text-embedding-ada-002"))
+    input = input_message
+    response = embedding.get_response(input: input)
+    puts response
+    thanks_message
+  end
+
   desc "image", "Image API"
   def image
     puts "Please input image you wish to generate."
     input = STDIN.gets.chomp
     client = OpenAI::Client.new
-    image = RubyOpenAI::Image.new(client, model_version)
+    image = RubyOpenAI::Image.new(client, model_version("babbage-similarity"))
     response = image.get_response(prompt: input)
     puts response
   end
